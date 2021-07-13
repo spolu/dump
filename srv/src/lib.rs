396933c148c2541d9dee65553a52cb261e3c2c50
Stanislas Polu <spolu@openai.com>
2021-07-13 16:27:33 +0200

    rust bindings

--- srv/src/lib.rs
+++ srv/src/lib.rs
@@ -13,18 +13,6 @@ lazy_static! {
         db::DB::new(Path::new(&env::var("HOME").unwrap()).join(".dump.db")).unwrap();
 }
 
-fn list_entries(query: &String, offset: usize, limit: usize) -> Result<models::EntryList> {
-    let (total, entries) = DB.list_entries(query, offset, limit).unwrap();
-
-    tracing::debug!(query = query.as_str(), offset, limit, total, "list_entries",);
-
-    Ok(models::EntryList {
-        entries,
-        total,
-        offset,
-    })
-}
-
 #[no_mangle]
 pub extern "C" fn response_free_ffi(response: *mut raw::c_char) {
     unsafe {
@@ -35,26 +23,154 @@ pub extern "C" fn response_free_ffi(response: *mut raw::c_char) {
     };
 }
 
+fn list_entries(options: models::ListOptions) -> Result<models::EntryList> {
+    let (total, entries) = DB
+        .list_entries(&options.query, options.offset, options.limit)
+        .unwrap();
+
+    tracing::debug!(
+        query = options.query.as_str(),
+        offset = options.offset,
+        limit = options.limit,
+        total,
+        "list_entries",
+    );
+
+    Ok(models::EntryList {
+        entries,
+        total,
+        offset: options.offset,
+    })
+}
+
+macro_rules! make_ffi {
+    ($function:expr, $request:expr, $type:ty) => {{
+        let request_c_str = unsafe { CStr::from_ptr($request) };
+        let json = match request_c_str.to_str() {
+            Err(err) => serde_json::to_string(&models::ErrorResponse {
+                error: format!("{}", err),
+            })
+            .unwrap(),
+            Ok(query) => {
+                let r: Result<$type, serde_json::Error> = serde_json::from_str(query);
+                match r {
+                    Err(err) => serde_json::to_string(&models::ErrorResponse {
+                        error: format!("{}", err),
+                    })
+                    .unwrap(),
+                    Ok(r) => serde_json::to_string(&$function(r).unwrap()).unwrap(),
+                }
+            }
+        };
+
+        CString::new(json.as_str()).unwrap().into_raw()
+    }};
+}
+
 #[no_mangle]
 pub extern "C" fn list_entries_ffi(request: *const raw::c_char) -> *mut raw::c_char {
-    let request_c_str = unsafe { CStr::from_ptr(request) };
-    let json = match request_c_str.to_str() {
-        Err(err) => serde_json::to_string(&models::ErrorResponse {
-            error: format!("{}", err),
-        })
-        .unwrap(),
-        Ok(query) => {
-            let r: Result<models::ListOptions, serde_json::Error> = serde_json::from_str(query);
-            match r {
-                Err(err) => serde_json::to_string(&models::ErrorResponse {
-                    error: format!("{}", err),
-                })
-                .unwrap(),
-                Ok(r) => serde_json::to_string(&list_entries(&r.query, r.offset, r.limit).unwrap())
-                    .unwrap(),
-            }
-        }
-    };
+    make_ffi!(list_entries, request, models::ListOptions)
+}
 
-    CString::new(json.as_str()).unwrap().into_raw()
+fn create_entry(create: models::Entry) -> Result<models::Entry> {
+    let entry = DB.create_entry(&create).unwrap();
+
+    tracing::debug!(
+        id = entry.id.clone().unwrap().as_str(),
+        created = entry.created.unwrap(),
+        title = entry.title.clone().as_str(),
+        meta = entry.meta.clone().as_str(),
+        "create_entry",
+    );
+
+    Ok(entry)
+}
+
+#[no_mangle]
+pub extern "C" fn create_entry_ffi(request: *const raw::c_char) -> *mut raw::c_char {
+    make_ffi!(create_entry, request, models::Entry)
+}
+
+fn update_entry(update: models::Entry) -> Result<models::Entry> {
+    let mut entry = DB.get_entry(&update.id.unwrap()).unwrap().unwrap();
+    entry.title = update.title;
+    entry.body = update.body;
+    entry.meta = update.meta;
+
+    DB.insert_entry(&entry).unwrap();
+
+    tracing::debug!(
+        id = entry.id.clone().unwrap().as_str(),
+        created = entry.created.unwrap(),
+        title = entry.title.clone().as_str(),
+        meta = entry.meta.clone().as_str(),
+        "update_entry",
+    );
+
+    Ok(entry)
+}
+
+#[no_mangle]
+pub extern "C" fn update_entry_ffi(request: *const raw::c_char) -> *mut raw::c_char {
+    make_ffi!(update_entry, request, models::Entry)
+}
+
+fn delete_entry(delete: models::Entry) -> Result<models::Entry> {
+    DB.delete_entry(&delete.id.clone().unwrap()).unwrap();
+
+    tracing::debug!(id = delete.id.clone().unwrap().as_str(), "delete_entry",);
+
+    Ok(delete)
+}
+
+#[no_mangle]
+pub extern "C" fn delete_entry_ffi(request: *const raw::c_char) -> *mut raw::c_char {
+    make_ffi!(delete_entry, request, models::Entry)
+}
+
+fn list_streams(_options: models::ListOptions) -> Result<models::StreamList> {
+    let streams: Vec<models::Stream> = DB.list_streams().unwrap();
+    let total = streams.len();
+
+    tracing::debug!(total, "list_streams",);
+
+    Ok(models::StreamList { streams, total })
+}
+
+#[no_mangle]
+pub extern "C" fn list_streams_ffi(request: *const raw::c_char) -> *mut raw::c_char {
+    make_ffi!(list_streams, request, models::ListOptions)
+}
+
+fn delete_stream(delete: models::Stream) -> Result<models::Stream> {
+    DB.delete_stream(&delete.id).unwrap();
+
+    tracing::debug!(id = delete.id.as_str(), "delete_stream",);
+
+    Ok(delete)
+}
+
+#[no_mangle]
+pub extern "C" fn delete_stream_ffi(request: *const raw::c_char) -> *mut raw::c_char {
+    make_ffi!(delete_stream, request, models::Stream)
+}
+
+fn update_stream(update: models::Stream) -> Result<models::Stream> {
+    let mut stream = DB.get_stream(&update.id).unwrap().unwrap();
+    stream.name = update.name;
+
+    DB.insert_stream(&stream).unwrap();
+
+    tracing::debug!(
+        id = stream.id.clone().as_str(),
+        name = stream.name.clone().as_str(),
+        "update_stream",
+    );
+
+    Ok(stream)
+}
+
+#[no_mangle]
+pub extern "C" fn update_stream_ffi(request: *const raw::c_char) -> *mut raw::c_char {
+    make_ffi!(update_stream, request, models::Stream)
 }
